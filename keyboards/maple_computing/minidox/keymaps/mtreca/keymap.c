11f4357c905afa31c88d52e1e9930d6ab85b752f
Maxime Tréca <maxime.treca@gmail.com>
2024-01-13 20:04:58 +0100

    Update Keyboards

--- keyboards/maple_computing/minidox/keymaps/mtreca/keymap.c
+++ keyboards/maple_computing/minidox/keymaps/mtreca/keymap.c
@@ -1,9 +1,10 @@
 #include QMK_KEYBOARD_H
 
 enum layer_names {
-    _QWERTY,
+    _ALPHA,
     _LOWER,
-    _RAISE
+    _RAISE,
+    _MOUSE
 };
 
 
@@ -24,16 +25,17 @@ enum layer_names {
 
 #define LOWTAB LT(_LOWER, KC_TAB) 
 #define RAIENT LT(_RAISE, KC_ENT)
+#define MSEESC LT(_MOUSE, KC_ESC)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
-[_QWERTY] = LAYOUT_split_3x5_3(
+[_ALPHA] = LAYOUT_split_3x5_3(
   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
   ALT_A,   CTL_S,   GUI_D,   SFT_F,   KC_G,         KC_H,    SFT_J,   GUI_K,   CTL_L,   ALT_C,
   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                    KC_ESC,  LOWTAB,  KC_SPC,       KC_BSPC, RAIENT,  KC_DEL
+                    MSEESC,  LOWTAB,  KC_SPC,       KC_BSPC, RAIENT,  KC_DEL
 ),
 
 [_RAISE] = LAYOUT_split_3x5_3(
@@ -46,6 +48,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_split_3x5_3(
   XXX,     XXX,     XXX,     XXX,     XXX,          XXX,     KC_HOME, KC_UP,   KC_END,  KC_PGUP,
   KC_LALT, KC_LCTL, KC_LGUI, KC_LSFT, XXX,          XXX,     KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,
+  XXX,     XXX,     XXX,     XXX,     XXX,          XXX,     XXX,     XXX,     XXX,     XXX,
+                    XXX,     ___,     XXX,          XXX,     XXX,     XXX
+),
+
+[_MOUSE] = LAYOUT_split_3x5_3(
+  XXX,     XXX,     XXX,     XXX,     XXX,          XXX,     KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U,
+  KC_LALT, KC_LCTL, KC_LGUI, KC_LSFT, XXX,          XXX,     KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,
   XXX,     XXX,     XXX,     XXX,     XXX,          XXX,     XXX,     XXX,     XXX,     XXX,
                     XXX,     ___,     XXX,          XXX,     XXX,     XXX
 )
